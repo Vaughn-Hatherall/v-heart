34a6863de99f9ff0f37e880d6e10769e04fcde88
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-17 12:10:40 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+	
+})
